78278d4d56bfbe428c04f41aa4f05e76c7aa9b90
Agyenim Boateng Barfi <aboateng62@gmail.com>
2026-03-10 21:06:40 +0000

    structured output

--- main.py
+++ main.py
@@ -5,8 +5,32 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
 from langchain_community.document_loaders import BraveSearchLoader
+from pydantic import BaseModel, Field
+from typing import List
 
 load_dotenv()
+
+
+class Source(BaseModel):
+    """
+    Args:
+        BaseModel (_type_): _description_
+    """
+
+    name: str = Field(..., description="The name of the source")
+    url: str = Field(..., description="The URL of the source")
+
+
+class AgentResponse(BaseModel):
+    """
+    Args:
+        BaseModel (_type_): _description_
+    """
+
+    answer: str = Field(..., description="The answer to the query")
+    sources: List[Source] = Field(
+        default_factory=list, description="The sources used to answer the query"
+    )
 
 
 @tool
@@ -26,15 +50,20 @@ def search(query: str) -> str:
 
 llm = ChatOllama(model="ministral-3:latest")
 tools = [search]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
     print("Hello from langchian!")
     result = agent.invoke(
-        {"messages": HumanMessage(content="search for 3 job postings for an ai engineer using langchain in Berlin and list their details")}
+        {
+            "messages": HumanMessage(
+                content="search for 3 job postings for an ai engineer using langchain in Berlin and list their details"
+            )
+        }
     )
     print(result)
+
 
 if __name__ == "__main__":
     main()
